b778f22e52a2570ba4c96e7f2084fb5e20603981
Lê Thành Chính <bulletz6105@gmail.com>
2026-01-27 15:52:58 +0700

    fix: resolve conflict between feature/gui brach with main branch

--- gui/windows.py
+++ gui/windows.py
@@ -3,23 +3,19 @@ from PIL import Image, ImageTk
 import cv2
 
 def show_result_popup(parent, cv_crop_img, text_content):
-    """
-    Hiển thị cửa sổ popup chứa ảnh biển số đã cắt và text
-    """
     if cv_crop_img is None or cv_crop_img.size == 0:
         return
 
-    # Tạo cửa sổ con (Toplevel)
+    #Tạo cửa sổ
     window = ctk.CTkToplevel(parent)
     window.title("Chi tiết biển số")
     window.geometry("400x350")
     
-    # Đảm bảo cửa sổ con nổi lên trên
+    #Đảm bảo cửa sổ con nổi lên trên
     window.transient(parent) 
     window.grab_set() 
 
-    # --- 1. Xử lý ảnh để hiển thị ---
-    # Resize ảnh crop cho to lên để dễ nhìn (ví dụ width = 300)
+    #Resize ảnh crop cho to lên để dễ nhìn (ví dụ width = 300)
     h, w, _ = cv_crop_img.shape
     target_w = 300
     ratio = target_w / w
@@ -29,7 +25,7 @@ def show_result_popup(parent, cv_crop_img, text_content):
     pil_img = Image.fromarray(img_rgb)
     ctk_img = ctk.CTkImage(light_image=pil_img, dark_image=pil_img, size=(target_w, target_h))
 
-    # --- 2. Giao diện Popup ---
+    #Giao diện Popup
     lbl_img = ctk.CTkLabel(window, text="", image=ctk_img)
     lbl_img.pack(pady=20)
     
